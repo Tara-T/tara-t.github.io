3c44327f02436d8f850ff4d7cde0b56bf8e9a71f
Tara-T <tara.trbos@gmail.com>
2020-02-28 09:47:37 +0100

    Dark/light mode fix

--- include/main.js
+++ include/main.js
@@ -1,14 +1,12 @@
 var checkbox = document.querySelector('input[name=mode]');
  
-        checkbox.addEventListener('change', function() {
-            if(this.checked) {
-                trans()
-                document.documentElement.setAttribute('data-theme', 'lighttheme')
-            } else {
-                trans()
-                document.documentElement.setAttribute('data-theme', 'darktheme')
-            }
-        })
+document
+  .getElementById("themeSwitch")
+  .addEventListener("change", function(event) {
+    event.target.checked
+      ? document.body.setAttribute("data-theme", "light")
+      : document.body.removeAttribute("data-theme");
+  });
  
         let trans = () => {
             document.documentElement.classList.add('transition');
@@ -16,3 +14,4 @@ var checkbox = document.querySelector('input[name=mode]');
                 document.documentElement.classList.remove('transition');
             }, 1000)
         }
+        
