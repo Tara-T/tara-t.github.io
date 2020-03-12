cf9b9fad501add2010093101ca191a4f0b3325fc
An Internet Troll <lucafulger@gmail.com>
2020-03-12 14:03:30 +0100

    Better support for translation

--- include/main.js
+++ include/main.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
-    var checkbox = document.querySelector('input[name=mode]');
-    checkbox.addEventListener('change', function() {
-        if (this.checked) {
-            trans()
-            document.documentElement.setAttribute('data-theme', 'lighttheme')
-        } else {
-            trans()
-            document.documentElement.setAttribute('data-theme', 'darktheme')
-        }
-    })
-    let trans = () => {
-        document.documentElement.classList.add('transition');
-        window.setTimeout(() => {
-            document.documentElement.classList.remove('transition');
-        }, 1000)
+  var checkbox = document.querySelector("input[name=mode]");
+  var norskID = document.getElementById("norsk");
+  checkbox.addEventListener("change", function() {
+    if (this.checked) {
+      trans();
+      document.documentElement.setAttribute("data-theme", "lighttheme");
+    } else {
+      trans();
+      document.documentElement.setAttribute("data-theme", "darktheme");
     }
+  });
+  let trans = () => {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+      document.documentElement.classList.remove("transition");
+    }, 1000);
+  };
+  norskID.style.display = "none";
 });
+
+function englishShow() {
+  var norskID = document.getElementById("norsk");
+  var englishID = document.getElementById("english");
+  norskID.style.display = "none";
+  englishID.style.display = "";
+}
+function norskShow() {
+  var norskID = document.getElementById("norsk");
+  var englishID = document.getElementById("english");
+  norskID.style.display = "";
+  englishID.style.display = "none";
+}
+
