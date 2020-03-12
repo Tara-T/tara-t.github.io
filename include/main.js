40f2fe94743bf93b08c6f5a27fa7ff093559511c
An Internet Troll <lucafulger@gmail.com>
2020-03-12 14:12:16 +0100

    Added support for Service Workers

--- include/main.js
+++ include/main.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    navigator.serviceWorker.register('/include/sw.js').then(function(registration) {
+      console.log('Service worker registration succeeded:', registration);
+    }, /*catch*/ function(error) {
+      console.log('Service worker registration failed:', error);
+    });
+  } else {
+    console.log('Service workers are not supported.');
+  }
+
 $(document).ready(function() {
   var checkbox = document.querySelector("input[name=mode]");
   var norskID = document.getElementById("norsk");
